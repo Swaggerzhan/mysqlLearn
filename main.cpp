a682fa3fd28430540fc70e187b3c7836dd70dd37
swagger <zsw18250205073@gmail.com>
2021-07-31 21:00:19 +0800

    redis client init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+extern "C"{
+#include "hiredis.h"
+}
+
+using std::cerr;
+using std::endl;
+using std::cout;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+    redisContext* redis = redisConnect("127.0.0.1", 6380);
+
+    if ( redis == nullptr ){
+        cerr << "connect to redis failed!" << endl;
+        exit(1);
+    }else {
+        cout << "success" << endl;
+    }
+
+    void* ret = redisCommand(redis, "hget hash C++");
+    redisReply* res = (redisReply*)ret;
+
+    if ( res->type == REDIS_REPLY_STRING ){
+        cout << "got String" << endl;
+        cout << "res: " << res->str << endl;
+    }
+
+
 }
